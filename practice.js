bad6ff9060a4f3b27b2b5ae32d2564b62c9568eb
alishbasheikhusman <152896627+alishbasheikhusman@users.noreply.github.com>
2023-12-15 23:14:27 +0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,304 @@
+// chap # 1
+
+// var firstName = ('my good name is alishba');
+// alert(firstName);
+// var lastName = ('my last name is sheikh');
+// alert(lastName);
+// var Email = ('aleeza.sheikh 789 @ gmail.com');
+// alert(Email);
+// var password = ('123456788');
+// alert(password);
+
+// qus # 2
+// alert("You're learning JavaScript!");
+
+// qus # 3
+
+// alert('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere libero vel quam blandit, ac cursus lacus dapibus');
+
+
+// Chapter 2 (Variables for string)
+
+// let ex = ('thisIsAlishba MyFatherNameIsUsman');
+// console.log(ex);
+
+// qus # 2
+
+// var hobby = '';
+// console.log('gym');
+
+// qus # 3
+
+// var teamName = ('my team member');
+// alert(teamName);
+// var teamName = ('usama');
+// alert(teamName);
+
+// var teamName = ('bisma');
+// alert(teamName);
+
+// var teamName = ('fayyaz');
+// alert(teamName);
+
+// var teamName = ('salman');
+// alert(teamName);
+
+
+//  qus # 4
+
+// var bestMan = 'myBaba'
+// console.log(bestMan);
+
+// Chapter 3 / qus no 1(Variables for numbers)
+
+// let caseQty = 5; // Declaration and assignment
+// console.log("Case Quantity:", caseQty);
+
+// qus # 2
+
+// let caseQty = 144;
+// console.log(caseQty);
+
+// qus # 3
+
+// let changeValue = ('9');
+// let num = Number(changeValue);
+// console.log(num);
+
+// // qus # 4
+// let number = 5;
+// let math = 6;
+// let ans = parseInt(number + math);
+// console.log(ans);
+
+// qus # 5
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+
+// qus # 6
+
+// let changeVar = 9;
+// changeVar = changeVar + 10;
+// console.log(changeVar);
+
+
+// Chapter 4 (Variable names Legal and Illegal)
+//   qus # 1
+
+// var productCost = 3.45;
+// console.log(productCost);
+
+// qus # 2
+// var NameOfBand = 19;
+// console.log(NameOfBand);
+// qus # 3
+// let alishba = 19;
+
+// qus # 4
+
+// let Nickname = ('my nick name is alishba');
+// console.log(Nickname);
+
+// qus # 5
+// Examples of legal variable names:
+
+// var myVariable;
+// var _underscoreVariable;
+// var variable123;
+// Examples of illegal variable names:
+
+// var 123illegal;  // Starts with a number
+// var special@char; // Contains a special character
+// var illegal space; // Contains a space
+
+// Chapter 5 (Math Expression I)
+//  qus # 1
+// operator name is modulus
+
+// var value = 6;
+// var modulus = 19;
+// var explain = 6 % 19;
+// console.log(explain);
+
+
+// qus # 2
+
+// var num = 20 % 6;
+
+// var num = 20;
+// var number = 6;
+// var operator = 20 % 6;
+// console.log(operator);
+
+// qus # 3
+
+// let largeNum = 1000 * 2000;
+// console.log(largeNum);
+
+// qus # 4
+
+// var variable_1 = 20;
+// var variable_2 = 10;
+// var result = (variable_1 - variable_2);
+// console.log(result);
+
+// qus # 5
+
+// var alishy = 12;
+// var alishba = 24;
+// var reminderValue = alishy % alishba;
+// console.log(reminderValue);
+
+// qus # 6
+
+// var multi = 2;
+// var play = 4;
+// var result = 2 * 4;
+// alert(result);
+
+
+// Chapter 12 (if…else and else if statements)
+
+// qus no 1
+
+// var age = 25;
+
+// if (age == 25) {
+//     console.log('age is 20');
+// } else {
+//     console.log('age is not 20');
+// };
+
+// var sub1 = parseInt(prompt('i got marks in englis'));
+// var sub2 = parseInt(prompt('i got marks in maths'));
+// var sub3 = parseInt(prompt('i got marks in science'));
+// var sub4 = parseInt(prompt('i got marks in economics'));
+// var sub5 = parseInt(prompt('i got marks in political science'));
+
+// var total = parseInt(sub1 + sub2 + sub3 + sub4 + sub5);
+// alert('your total marks is 500 out of by ' + total);
+
+// var per = parseInt(alert('your percentage is ' + total / 500 * 100));
+
+// if (per <= 99 || 80) {
+//     alert('you are paseed')
+// } else if (per == 79 || 70) {
+//     alert('your result is poor');
+
+// } else {
+//     alert('please work hard')
+//         ;
+// }
+
+// qus no 2
+
+// var a = 10;
+// if (a === 10) {
+//     alert('a is 10')
+
+// } else {
+//     alert('please correct value of a')
+// };
+
+// qus # 3
+
+// var city = ('jhudo');
+// if (city === ('karachi')) {
+//     alert('This is Karachi. Welcome to the city of lights! ')
+
+// } else if (city === ('lahore')) {
+//     alert('This is Lahore. Welcome to the heart of Pakistan!')
+// }
+
+// {
+//     alert('This is a city, but not Karachi or Lahore.')
+
+// };
+
+// let age = 40;
+
+// if (age >= 25 && age <= 40) {
+//     alert('you are inagible')
+
+// } else {
+//     alert('you are not inagible')
+
+// };
+
+// Chapter 15 (Array I)
+
+// qus no 1
+
+// let empty = [];
+// console.log(empty);
+
+// qus # 2
+
+// let string = ['apple'];
+// console.log(string);
+
+// qus # 3
+
+// var alphabet = ["h", "i", "j", "k"];
+// console.log(alphabet[2]);
+
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// var arryLength = alphabet.length;
+// console.log(arryLength);
+
+// qus no # 4
+
+// let myArray = ['my name is alishba'];
+// myArray[0] = 'my father name is usman';
+// console.log(myArray[0]);
+
+// Chapter 16 (Array II)
+
+// var abc = ['alishba', 'usama', 'bisma', 'fayyaz', 'sana', 'hunain'];
+
+// abc.push('nahid');
+
+// alert(abc);
+
+// for adding some straing and numaric value,
+
+// qus # 2
+
+// var def = ['apple', 'banana', 'orange', 'mango', 'watermelon', 'gava'];
+
+// def.pop();
+// console.log(def);
+
+
+// qus # 3
+// var Alphabet = ["h", "i", "j", "k"];
+
+// Alphabet.push(42);
+// console.log(Alphabet);
+
+// Chapter 16 (Array III);
+
+// qus no 1
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+// sizes.shift();
+
+// alert(sizes);
+
+// qus # 2
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+// sizes.unshift(1, 2, 3);
+// alert(sizes);
+
+// qus # 3
+let first = ['alishba-usman'];
+first.unshift('my name is alishba');
+console.log(first[0]);
+
+
